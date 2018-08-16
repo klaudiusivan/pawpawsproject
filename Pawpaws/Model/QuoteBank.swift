d6c5fb8dc81a9f088cb96f457a7dd32c0e676763
Klaudius Ivan <klaudsvanz@gmail.com>
2018-08-16 17:46:05 +0700

    Set Quote

---
+++ Pawpaws/Model/QuoteBank.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteBank.swift
+//  Pawpaws
+//
+//  Created by Klaudius Ivan on 16/08/18.
+//  Copyright © 2018 Klaudius. All rights reserved.
+//
+
+import Foundation
+
+class QuoteBank{
+    var quoteList: [String] = []
+    
+    init(){
+        quoteList.append("Time spent with cats is never wasted. - Sigmund Freud")
+    }
+}
